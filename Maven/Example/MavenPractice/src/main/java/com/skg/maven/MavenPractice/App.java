40f40320dce17b5df80ac95667a4b8d323f9680c
Khawja Ghouse <31949115+khawja-ghouse@users.noreply.github.com>
2022-07-21 17:51:51 +0530

    Updating Maven doc and adding Maven Example

---
+++ Maven/Example/MavenPractice/src/main/java/com/skg/maven/MavenPractice/App.java
@@ -0,0 +1,15 @@
+package com.skg.maven.MavenPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("---------------Testing Plugin Example ---------");
+		System.out.println("App Main programs runs at compile phase");
+		System.out.println("Arguments passed from Plugin : " + args[0] + "," + args[1]);
+		System.out.println("---------------End Of App program -------------");
+	}
+}

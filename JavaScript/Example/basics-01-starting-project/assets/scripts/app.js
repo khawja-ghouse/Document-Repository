db5ef42c106c4f1a55616fb469dc6d953c908741
Khawja Ghouse <31949115+khawja-ghouse@users.noreply.github.com>
2023-02-06 11:29:09 +0530

    Updated JS doc

--- JavaScript/Example/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/Example/basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,15 @@
-let currentResult = 0;
+const defaultResult = 0;
 
-currentResult = currentResult + 10;
+let currentResult = defaultResult;
 
-outputResult(currentResult,'');
+currentResult = (currentResult + 10) * 3 / 2 -1;
+
+//let calculationDescription = ' ( ' +defaultResult + ' + 10) * 3 / 2 -1';
+
+// Whats the use of `` --> it avoids the use of manual string concatination with variables so the above line can also be written as
+
+let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 -1`;
+
+// using `` is called as template literal in JavaScript
+
+outputResult(currentResult,calculationDescription);
